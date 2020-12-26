27e625e9e6e619167b238c6c9ff1e435d91590d6
antoniojh10 <antoniohernandez95@gmail.com>
2020-12-26 19:05:15 -0400

    Hooks

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
-import { RiHeartLine } from 'react-icons/ri';
-import { ImgWrapper, Img, Button } from './styles';
+import React, { useState, useEffect, useRef } from 'react';
+import { RiHeartFill, RiHeartLine } from 'react-icons/ri';
+import { Article, ImgWrapper, Img, Button } from './styles';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useNearScreen } from '../../hooks/useNearScreen';
 
 const DEFAULT_SRC =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_SRC }) => (
-  <article>
-    <a href={`/detail/${id}`}>
-      <ImgWrapper>
-        <Img src={src} alt={id} />
-      </ImgWrapper>
-    </a>
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_SRC }) => {
+  const [show, element] = useNearScreen();
+  const key = `like-${id}`;
+  const [liked, setLiked] = useLocalStorage(key, false);
 
-    <Button type="button">
-      <RiHeartLine size="2rem" />
-      {likes} likes!
-    </Button>
-  </article>
-);
+  return (
+    <Article ref={element}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt={id} />
+            </ImgWrapper>
+          </a>
+
+          <Button type="button" onClick={() => setLiked(!liked)}>
+            {liked ? (
+              <RiHeartFill color="red" size="2rem" />
+            ) : (
+              <RiHeartLine size="2rem" />
+            )}
+            {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
+  );
+};
