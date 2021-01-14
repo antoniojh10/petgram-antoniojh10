ebf889033689f39c4cb453b95412ddae7adf5295
antoniojh10 <antoniohernandez95@gmail.com>
2021-01-13 22:21:03 -0400

    Tests with cypress and final

--- cypress/integration/petgram/test_spec.js
+++ cypress/integration/petgram/test_spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable func-names */
 /* eslint-disable prefer-arrow-callback */
-/* global describe, it, cy */
+/* global describe, it, cy, Cypress */
 
 describe('Petgram', function () {
   it('Para ver si la app funciona', function () {
@@ -10,5 +10,20 @@ describe('Petgram', function () {
   it('Navegamos a una categoria y vemos fotos', function () {
     cy.visit('/pet/1');
     cy.get('article');
+  });
+
+  it('Si podemos navegar con la navbar a la home', function () {
+    cy.visit('/pet/1');
+    cy.get('nav a').first().click();
+    cy.url().should('eq', Cypress.config().baseUrl);
+  });
+
+  it('Los usuarios no registrados ven el formulario de registro e inicio de sesión al ir a favs o user', function () {
+    cy.visit('/');
+    cy.get('nav a').eq(1).click();
+    cy.get('form').should('have.length', 2);
+
+    cy.get('nav a').eq(2).click();
+    cy.get('form').should('have.length', 2);
   });
 });
